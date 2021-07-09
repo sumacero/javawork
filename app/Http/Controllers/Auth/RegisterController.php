971a8068770cba1d6d665ef4a1124a5b6aaac449
sumacero <za100c80s116@gmail.com>
2021-07-09 16:01:34 +0900

    認証機能の仮実装

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'authority_id' => '2',
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
