044fe3d9b7c39e5fe778e8102ce76bc6753aa4f7
sumacero <za100c80s116@gmail.com>
2021-07-12 16:29:17 +0900

    Reactのインストール

---
+++ resources/views/search.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <p>ここは問題検索画面です</p>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
+Route::get('/search', 'SearchController@index');
